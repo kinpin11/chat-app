642dbc6787d85f5add41b9cdcddb319130218ae5
yoppy-luppy <ysk11knst@gmail.com>
2020-07-24 18:05:35 +0900

    チャットルームの削除機能の実

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message= Message.new
     @room = Room.find(params[:room_id])
-    @messages = @room.messages.include(:user)
+    @messages = @room.messages.includes(:user)
   end
   def create
    @room = Room.find(params[:room_id])
@@ -13,6 +13,7 @@ class MessagesController < ApplicationController
       render :index
    end
   end
+
 
 
 private
